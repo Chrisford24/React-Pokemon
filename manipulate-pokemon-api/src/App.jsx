ba1a0fba51c4f8450139c9dd151e6110ba3d9921
Vonney <davonnejv@gmail.com>
2023-07-10 17:21:22 -0500

    trying to show pokemon images

--- manipulate-pokemon-api/src/App.jsx
+++ manipulate-pokemon-api/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   const [pokemonData, setPokemonData] = useState(null);
+  const[pokemonSprite, getSprite] = useState(null);
 
   const POKEMON_API = "https://pokeapi.co/api/v2/pokemon?limit=1000";
 
@@ -22,22 +23,33 @@ function App() {
 
   },[]);
 
+  // useEffect(() => {
+  //   const fetchImages = async () => {
+  //     const response = await fetch("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites")
+  //     const getResults = await response.json();
+  //     getSprite(getResults);
+  //   }
+  //   fetchImages();
+  //  }, []);
+
   return (
     <>
       <div className="main">
         <h1>Pokemon</h1>
 
         {pokemonData && (
-          <div className="pokemon-container">
+           <div className="pokemon-container">
             {pokemonData.results.map((pokemon) => (
               <div key={pokemon.name}>
                 <h2>{pokemon.name.toUpperCase()}</h2>
-                <img src={pokemon.sprites} alt={pokemon.name}/>
+                <img src={pokemon.sprite} alt="Pokemon Image" />
               </div>
             ))}
           </div>
         )}
-      </div>
+      </div> {/*/ last div for main */}
+      
+
     </>
   )
 }
